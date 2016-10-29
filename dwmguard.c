5d821d7b36c235a7fb0463adcebda218868c6c30
Alex Kozadaev <akozadaev at yahoo com>
2016-10-29 11:15:41 +0100

    makefiles and style changes

--- dwmguard.c
+++ dwmguard.c
@@ -26,18 +26,19 @@ main(void)
                 execl(DWM, DWM, NULL);
                 perror("execv");
                 exit(1);
-            default:
-                if (wait(&status) < 0) {
-                    perror("wait");
-                    exit(1);
-                }
-                if (WIFEXITED(status)) {
-                    running = (WEXITSTATUS(status) != 0);
-                } else if (WIFSIGNALED(status)) {
-                    printf("process %d killed: signal %d\n", pid,
-                           WTERMSIG(status));
-                    exit(WTERMSIG(status));
-                }
+        }
+
+        if (wait(&status) < 0) {
+            perror("wait");
+            exit(1);
+        }
+
+        if (WIFEXITED(status)) {
+            running = (WEXITSTATUS(status) != 0);
+        } else if (WIFSIGNALED(status)) {
+            printf("process %d killed: signal %d\n", pid,
+                   WTERMSIG(status));
+            exit(WTERMSIG(status));
         }
     }
 
